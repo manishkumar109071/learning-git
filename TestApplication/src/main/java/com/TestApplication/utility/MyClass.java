8ec62f36a8a02f30888f0bd674f5d22ca076082f
manishkumar109071 <manishkumar109071@gmail.com>
2022-04-02 19:39:16 +0530

    hello

---
+++ TestApplication/src/main/java/com/TestApplication/utility/MyClass.java
@@ -0,0 +1,14 @@
+package com.TestApplication.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		List<String> arrayList = new ArrayList();
+		System.out.println();
+
+	}
+
+}
